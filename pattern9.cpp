51261b5ba3b0cb5fbe36d353d179e1f54a3ef35a
indranil Bakshi <iamneel67@gmail.com>
2024-07-10 23:59:32 +0530

    neel commits

---
+++ pattern9.cpp
@@ -0,0 +1,23 @@
+//ABC
+//ABC
+//ABC
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    for(int i=1; i<=n; i++){
+
+        for(int j=1; j<=n; j++){
+
+            char ch = 'A'+j-1;
+            cout<<ch;
+        }
+        cout<<endl;
+    }
+}
